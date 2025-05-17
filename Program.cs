02992b75739e318e2e4713714c11485c6c2955b4
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-16 22:11:07 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+string Sexo;
+double altura;
+Console.Write("Digite sua altura em m..........:");
+altura = Convert.ToDouble(Console.ReadLine());
+Console.Write("Sexo [M]asculino / [F]eminino...:");
+Sexo = Console.ReadLine().ToUpper()!;
+if (Sexo == "M")
+{
+    double PesoIdeal;
+    PesoIdeal = (altura * 72.7) - 58.0;
+    Console.Write($"Seu peso ideal é {PesoIdeal:N1}");
+
+}
+else if (Sexo == "F")
+{
+    double PesoIdeal;
+    PesoIdeal = (altura * 62.1) - 44.7;
+    Console.Write($"Seu peso ideal é {PesoIdeal:N1}");
+}
